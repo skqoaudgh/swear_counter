2c9d08ff9c59e7a1c6e174c9022e00cccb66e1a2
배명호(goorm) <mac.bae@goorm.io>
2022-07-26 13:16:46 +0000

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="App">
+            <div>욕하지마</div>
+        </div>
+    );
+}
+
+export default App;
